ee06ede6b6659c0c63b154a818d6440173b7dd73
faridggl100@gmail.com <faridggl100@gmail.com>
2022-04-15 23:18:48 +0200

    add deploy to circleci config.yml

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,3 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+aws s3 mb s3://contacts-frontend-bucket
+aws s3 website s3://contacts-frontend-bucket --index-document index.html
+aws s3 cp --recursive --acl public-read ./build s3://contacts-frontend-bucket

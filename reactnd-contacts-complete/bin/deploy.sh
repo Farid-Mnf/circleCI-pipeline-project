92e200a5a5b57e4f1f1573783a1ed9216cf996dc
faridggl100@gmail.com <faridggl100@gmail.com>
2022-04-16 22:49:51 +0200

    fix

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,7 +1,7 @@
-aws configure set aws_access_key_id AKIAXDRZ5VZGMUKM3UXZ
-aws configure set aws_secret_access_key 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
-aws configure get aws_access_key_id
-aws configure get aws_secret_access_key
+# aws configure set aws_access_key_id AKIAXDRZ5VZGMUKM3UXZ
+# aws configure set aws_secret_access_key 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
+# aws configure get aws_access_key_id
+# aws configure get aws_secret_access_key
 aws s3 mb s3://contacts-frontend-bucket
 aws s3 website s3://contacts-frontend-bucket --index-document index.html
 aws s3 cp --recursive --acl public-read ./build s3://contacts-frontend-bucket

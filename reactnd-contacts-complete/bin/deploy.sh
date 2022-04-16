0f34bd5fb177c50526d11ab314c33adcf53adb69
faridggl100@gmail.com <faridggl100@gmail.com>
2022-04-16 21:13:18 +0200

    fix

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -2,4 +2,5 @@ aws s3 mb s3://contacts-frontend-bucket
 aws s3 website s3://contacts-frontend-bucket --index-document index.html
 ls -a
 cat ~/.aws/credentials
+aws configure list
 aws s3 cp --recursive --acl public-read ./build s3://contacts-frontend-bucket

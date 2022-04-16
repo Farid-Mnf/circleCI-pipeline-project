868f32a822e607998e95abe69366f06df6b839d2
faridggl100@gmail.com <faridggl100@gmail.com>
2022-04-16 21:27:35 +0200

    fix

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,6 +1,5 @@
 aws s3 mb s3://contacts-frontend-bucket
 aws s3 website s3://contacts-frontend-bucket --index-document index.html
-ls -a
-cat ~/.aws/credentials
-aws configure list
+echo $AWS_ACCESS_KEY
+echo $AWS_SECRET_KEY
 aws s3 cp --recursive --acl public-read ./build s3://contacts-frontend-bucket
